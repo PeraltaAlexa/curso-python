8a79d85a5f1df83f9e1ad02abae9bec6fa7ee78b
Udem <udem@fedora.mshome.net>
2017-06-14 17:42:27 -0600

    juego

---
+++ intentofallido.py
@@ -0,0 +1,15 @@
+import random
+opciones {
+    'piedra':{
+	'papel': False,
+	'tijera': True,
+},
+'papel':{
+	'piedra':True,
+	'tijera',:False,
+},
+'tijera':{
+	'piedra': False,
+	'papel': True,
+  } 
+}
