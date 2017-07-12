48697f1ae0e4c4cb0a2eba094019c69ae7f0d569
Udem <udem@fedora.mshome.net>
2017-07-12 16:46:13 -0600

    ventana4

---
+++ python-gt/ventana4.py
@@ -0,0 +1,74 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class MiVentana(Gtk.Window):
+	def __init__(self, *args, **Kwargs):
+		super(MiVentana, self).__init__(*args, **Kwargs)
+		self.set_default_size(500,300)
+		self.connect('delete-event', Gtk.main_quit)
+		self.agregar_contenedor()
+		self.agregar_entrada()
+		self.agregar_boton()
+
+	def agregar_contenedor(self):
+		self.contenedor = Gtk.Grid()
+		self.contenedor.set_column_homogeneous(True)
+		self.add(self.contenedor)
+
+	def agregar_boton(self):
+		self.boton = Gtk.Button('inicio')
+		self.contenedor.attach_next_to(
+		self.boton,
+		self.entrada,
+		Gtk.PositionType.BOTTOM,
+		1,
+		1
+
+		)
+
+  		self.boton.connect('clicked', self.agregar_fila)
+
+	def agregar_lista(self):
+		self.modelo = Gtk.ListStore(str, float)
+  		self.lista_activos = Gtk.TreeView(self.modelo)
+    		descripcion = Gtk.CellRendererText()
+      		columna_descripcion = Gtk.TreeViewColumn(
+      		    'Descripcion',
+            	descripcion,
+             	text=0
+      		)
+		monto = Gtk.CellRendererText()
+  		columna_monto = Gtk.TreeViewColumn('Monto', monto, text=1)
+
+		self.lista_activos.append_column(columna_descripcion)
+		self.lista_activos.append_column(columna_monto)
+
+		self.contenedor.attach_next_to(
+		self.lista_activos,
+		self.boton,
+		Gtk.PositionType.BOTTOM,
+		1,
+		1
+
+		)
+
+	def agregar_fila(self, btn):
+     		texto = self.entrada.get_text()
+       		self.modelo.append([texto, 1.5])
+
+
+
+	def agregar_entrada(self):
+                                                                                                                                                      		self.entrada = Gtk.Entry()
+		self.contenedor.attach(self.entrada,0,0,1,1)
+
+	def label(self):
+		etiqueta = Gtk.label()
+		etiqueta.set_markup('texto a ingresar')
+
+if __name__ == '__main__':
+	ventana = MiVentana()
+	ventana.show_all()
+
+	Gtk.main()
